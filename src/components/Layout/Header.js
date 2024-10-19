fc3507b223cb31e16d59a113e55c36612d4b8d65
hadil <hadilbennaceur1234@gmail.com>
2024-10-19 14:36:34 +0200

    Premier commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,129 @@
+import React, { useState } from "react";
+import {
+  AppBar,
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import Logo from "../../images/logo.jpg"; 
+
+import MenuIcon from "@mui/icons-material/Menu";
+import { NavLink } from "react-router-dom";
+import "../../styles/HeaderStyles.css";
+
+const Header = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  // Handle menu click
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  // Menu drawer for mobile view
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography
+        color={"goldenrod"}
+        variant="h6"
+        component="div"
+        sx={{ flexGrow: 1, my: 2 }}
+      >
+        <img src={Logo} alt="logo" height={"70"} width="200" />
+      </Typography>
+      <Divider />
+      <ul className="mobile-navigation">
+        <li>
+          <NavLink activeClassName="active" to={"/"}>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to={"/menu"}>Menu</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/about"}>About</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/contact"}>Contact</NavLink>
+        </li>
+      </ul>
+    </Box>
+  );
+
+  return (
+    <>
+      <Box>
+        <AppBar component={"nav"} sx={{ bgcolor: "black" }}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              sx={{
+                mr: 2,
+                display: { sm: "none" }, // Hide on larger screens
+              }}
+              onClick={handleDrawerToggle}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              color={"goldenrod"}
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1 }}
+            >
+              <img src={Logo} alt="logo" height={"70"} width="250" />
+            </Typography>
+            <Box sx={{ display: { xs: "none", sm: "block" } }}> {/* Show on larger screens */}
+              <ul className="navigation-menu">
+                <li>
+                  <NavLink activeClassName="active" to={"/"}>
+                    Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to={"/menu"}>Menu</NavLink>
+                </li>
+                <li>
+                  <NavLink to={"/about"}>About</NavLink>
+                </li>
+                <li>
+                  <NavLink to={"/contact"}>Contact</NavLink>
+                </li>
+              </ul>
+            </Box>
+          </Toolbar>
+        </AppBar>
+
+        {/* Drawer for mobile view */}
+        <Box component="nav">
+          <Drawer
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            sx={{
+              display: { xs: "block", sm: "none" }, // Only show on mobile
+              "& .MuiDrawer-paper": {
+                boxSizing: "border-box",
+                width: "240px",
+              },
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Box>
+
+        {/* Toolbar spacer to ensure content isn't hidden behind the AppBar */}
+        <Box>
+          <Toolbar />
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default Header;
